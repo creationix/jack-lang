637d0f278459aa1130ab96c063fcaf0611507e47
Tim Caswell <tim@creationix.com>
2015-01-29 16:08:37 -0600

    Tweak for benchmark.  fib(0-91) 1987925/second

--- main.c
+++ main.c
@@ -14,22 +14,25 @@ static intptr_t jack_fib(intptr_t n) {
 }
 
 int main() {
-  for (int j = 0; j < 0x1; ++j) {
+  state = jack_new_state(20);
+  jack_call(state, jack_math, 0);
+  jack_map_get_symbol(state, -1, "fib");
+  jack_new_list(state);
+  // 1 - fib
+  // 2 - list
 
-    state = jack_new_state(15);
-    jack_call(state, jack_math, 0);
-    jack_map_get_symbol(state, -1, "fib");
-    jack_new_list(state);
-    // 1 - fib
-    // 2 - list
+  for (int j = 0; j < 0x10000; ++j) {
 
+    printf("Generation %d\n", j);
     for (intptr_t i = 0; i <= 91; ++i) {
-      printf("fib(%ld) = %ld\n", i, jack_fib(i));
-      jack_list_push(state, 2);
+      jack_fib(i);
+      // printf("fib(%ld) = %ld\n", i, jack_fib(i));
+      jack_pop(state);
+      // jack_list_push(state, 2);
     }
-    jack_dump_state(state);
 
-    jack_free_state(state);
   }
+  jack_dump_state(state);
+  jack_free_state(state);
   return 0;
 }
